a3ef92f25cc6cc6b00bbd3175a910a1987da321f
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-08-04 22:16:34 +0530

    DataIngestion

--- Project1/src/components/data_ingestion.py
+++ Project1/src/components/data_ingestion.py
@@ -1,1 +1,24 @@
 #Code related to reading the data
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path : str = os.path.join('Artifact',"train.csv")
+    test_data_path : str = os.path.join('Artifact',"test.csv")
+    raw_data_path : str = os.path.join('Artifact',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.dataingestion=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Method")
+        try:
+            df=pd.read_csv("Project1\notebook\StudentsPerformance.csv")
+            
