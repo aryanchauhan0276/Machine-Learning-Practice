b77c3143824fc7df76460fec4ca2ce7d5141bc5c
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-08-05 12:45:31 +0530

    Data Ingestion Object Created

--- Project1/src/components/data_ingestion.py
+++ Project1/src/components/data_ingestion.py
@@ -15,10 +15,30 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.dataingestion=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Data Ingestion Method")
         try:
             df=pd.read_csv("Project1\notebook\StudentsPerformance.csv")
+            os.makedirs((self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw,index=False,header=True)
+            logging.info("train_test_split")
+            train_set,test_set=train_test_split(df,test_size=0.20,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            return (
+                self.ingestion_config.train_data_path
+                self.ingestion_config.test_data_path
+            )
+
+
+        except CustomException as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+
             
