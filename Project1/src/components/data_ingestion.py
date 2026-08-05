d4cfb96306f4edb56e0cd8f6e27b5fa9cad19522
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-08-05 12:46:02 +0530

    Data Ingestion Object Created

--- Project1/src/components/data_ingestion.py
+++ Project1/src/components/data_ingestion.py
@@ -28,10 +28,8 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            return (
-                self.ingestion_config.train_data_path
-                self.ingestion_config.test_data_path
-            )
+            return (self.ingestion_config.train_data_path
+                ,self.ingestion_config.test_data_path)
 
 
         except CustomException as e:
