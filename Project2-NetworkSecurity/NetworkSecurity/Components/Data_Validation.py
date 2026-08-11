084eefbe63ede41c3fe832c011b428eb59b643f9
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-08-11 21:21:47 +0530

    Data Validation Completed

---
+++ Project2-NetworkSecurity/NetworkSecurity/Components/Data_Validation.py
@@ -0,0 +1,92 @@
+from NetworkSecurity.Components import data_ingestion
+from NetworkSecurity.entity import config_entity
+from NetworkSecurity.exception.exception import NetworkSecurityException
+from NetworkSecurity.logging.logger import logging
+import os
+import sys
+import pandas as pd
+from NetworkSecurity.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
+from NetworkSecurity.entity.config_entity import DataValidationConfig
+from scipy.stats import k2_2samp
+from NetworkSecurity.Constants.training_pipeline import SCHEMA_FILE_PATH
+from NetworkSecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
+class DataValidation:
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,data_validation_config:DataValidationArtifact):
+        try:
+            self.data_ingestion_artifact=data_ingestion_artifact
+            self.data_validation_config=data_validation_config
+            self._schema_config=read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    @staticmethod
+    def read_data(file_path)-> pd.Dataframe:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def validate_numberofcols(self,dataframe : pd.DataFrame) -> bool:
+        try:
+            numberofcols=len(self._schema_config)
+            logging.info(f"Required No. of Cols:{numberofcols}")
+            logging.info(f"DataFrame No. of Cols:{len(dataframe.columns)}")
+            if len(dataframe.columns)==numberofcols:
+                return True
+            return False
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        try:
+            staus=True
+            report={}
+            for column in base_df.columns:
+                d1=base_df[column]
+                d2=current_df[column]
+                is_same_dist=k2_2samp(d1,d2)
+                if threshold <=is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found=True
+                    status=False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_staus":is_found
+                }})
+            drift_report_file_path=self.data_validation_config.drift_report_file_path
+            os.makedirs(drift_report_file_path,exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path,content=report)
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def initiate_data_validation(self)->DataValidationArtifact:
+        try:
+            train_file_path=self.data_ingestion_artifact.trained_file_path
+            test_file_path=self.data_ingestion_artifact.test_file_path
+
+            #readdataframtrain-test
+            train_dataframe=DataValidation.read_data(train_file_path)
+            test_dataframe=DataValidation.read_data(test_file_path)
+            #Validatenumberofcols
+            status=self.validate_numberofcols(dataframe=train_dataframe)
+            if not status:
+                error_message=f"{error_message} Train Dataframe does not contain all cols"
+
+            status=self.validate_numberofcols(dataframe=test_dataframe)
+            if not status:
+                error_message=f"{error_message} Test Dataframe does not contain all cols"  
+
+            status=self.detect_dataset_drift(base_df=train_dataframe,current_df=test_dataframe)        
+            dir_path=os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path,index=False,header=True
+            )
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path,index=False,header=True
+            )            
+        except Exception as e:
+            raise NetworkSecurityException
--- Project2-NetworkSecurity/main.py
+++ Project2-NetworkSecurity/main.py
@@ -1,8 +1,9 @@
 from NetworkSecurity.Components.data_ingestion import DataIngestion
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.exception.exception import NetworkSecurityException
-from NetworkSecurity.entity.config_entity import DataIngestionConfig
+from NetworkSecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
+from NetworkSecurity.Components.Data_Validation import DataValidation
 import sys
 if __name__=="__main__":
     try:
@@ -12,5 +13,10 @@ if __name__=="__main__":
         logging.info("Running Data Ingestion")
         dataingestion_artifact=data_ingestion.initiate_data_ingestion()
         print("Working Fine")
+        logging.info("Data Initiation Completed")
+        DataValidation(data_inges_config)
+        data_valid_config=DataValidationConfig(data_training_config)
+        data_valid=DataValidation(dataingestion_artifact,data_valid_config)
+        data_valid_artifact=data_valid.initiate_data_validation()
     except Exception as e:
         raise NetworkSecurityException(e,sys)
