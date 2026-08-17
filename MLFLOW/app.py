4cd8159fed9a960046b5db1dbb32b82df3163cab
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-08-17 20:16:54 +0530

    app.py updated

--- MLFLOW/app.py
+++ MLFLOW/app.py
@@ -1,7 +1,3 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
-
 import os
 import warnings
 import sys
@@ -75,10 +71,6 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        #predictions = lr.predict(train_x)
-        #signature = infer_signature(train_x, predictions)
-
-        ## For Remote server only(DAGShub)
 
         remote_server_uri="https://dagshub.com/krishnaik06/mlflowexperiments.mlflow"
         mlflow.set_tracking_uri(remote_server_uri)
