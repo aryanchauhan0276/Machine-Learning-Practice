3c694fea11854e6c30007c74f35ced37d742ae3b
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-09-02 19:49:36 +0530

    practice session

--- PractisingStructuredCodes/Project1/config/config_entiy.py
+++ PractisingStructuredCodes/Project1/config/config_entiy.py
@@ -1,5 +1,5 @@
 from constants import training_pipeline
-class utils_entity:
+class training_pipeline_config:
     def __init__(self,training_pipeline):
         self.artifact_file_path=training_pipeline.Artifact_File_Path
         self.dataset_path=training_pipeline.Dataset_Path
