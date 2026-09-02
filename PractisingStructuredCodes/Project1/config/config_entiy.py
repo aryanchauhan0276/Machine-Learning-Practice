3cbc2263f9340e24683a1abc76240d915eaa9640
Aryan Chauhan <aryanchauhan0276@gmail.com>
2026-09-02 20:00:34 +0530

    Add comment to config_entiy.py
    Added a comment to indicate this is practice code.

--- PractisingStructuredCodes/Project1/config/config_entiy.py
+++ PractisingStructuredCodes/Project1/config/config_entiy.py
@@ -3,4 +3,4 @@ class training_pipeline_config:
     def __init__(self,training_pipeline):
         self.artifact_file_path=training_pipeline.Artifact_File_Path
         self.dataset_path=training_pipeline.Dataset_Path
-   
+    #This is practice code
