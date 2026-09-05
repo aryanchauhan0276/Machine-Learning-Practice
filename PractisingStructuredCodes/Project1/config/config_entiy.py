928310a8b7eeff39b192fccf897baf63455082e9
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-09-05 17:41:38 +0530

    More structured codes

--- PractisingStructuredCodes/Project1/config/config_entiy.py
+++ PractisingStructuredCodes/Project1/config/config_entiy.py
@@ -9,5 +9,7 @@ class training_pipeline_config:
    
 class data_ingestion_config:
     def __init__(self,training_pipeline_config):
+        self.data_ingestion_dir=os.path.join(training_pipeline_config.artifact_file_path,training_pipeline.Data_Ingested_Dir)
+        self.data_ingestion_path=os.path.join(self.data_ingestion_dir,training_pipeline.Data_Ingested)
         self.training_dataset=os.path.join(training_pipeline_config.artifact_file_path,training_pipeline.Training_Data_Path)
         self.test_dataset=os.path.join(training_pipeline_config.artifact_file_path,training_pipeline.Test_Data_Path)
