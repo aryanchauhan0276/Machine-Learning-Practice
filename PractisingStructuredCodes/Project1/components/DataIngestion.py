1dbceb92f9494cbe06aa8fd19902b5050fc71a55
aryanchauhan0276 <aryanchauhan0276@gmail.com>
2026-09-12 12:25:53 +0530

    RNN Implementation

--- PractisingStructuredCodes/Project1/components/DataIngestion.py
+++ PractisingStructuredCodes/Project1/components/DataIngestion.py
@@ -1,6 +1,11 @@
 import os
-import practisingstructuredcodes.Project1.config.config_entity
+from Project1.config.config_entity import data_ingestion_config
 import sys
+from Exception.exception import exception
+from Logging.logging import logger
 class DataIngestion:
-    def __init__(self,config_entity):
-        pass
+    def __init__(self,Data_ingestion_config : data_ingestion_config):
+        try:
+            self.Data_Ingestion_config=data_ingestion_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
